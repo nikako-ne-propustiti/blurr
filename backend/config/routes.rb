d487d71d218e3a08d5bdd5a2d729723d2c1e5217
Luka Simić <git@kocka.tech>
2022-03-14 15:46:19 +0100

    Create login endpoint, remove sample controller.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'greetings/hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'static#index'
   get '/:username', to: 'static#index'
   get '/p/:post', to: 'static#index'
   get '/accounts/login', to: 'static#index'
   get '/accounts/edit', to: 'static#index'
+  post '/api/accounts/login', to: 'accounts#login'
 end
