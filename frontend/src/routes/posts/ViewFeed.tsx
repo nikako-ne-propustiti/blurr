b71def0feb77f7d7431126bd870737d7f3455729
Luka Simić <git@kocka.tech>
2022-03-14 15:49:41 +0100

    Redirect to the login form from the main feed.

--- frontend/src/routes/posts/ViewFeed.tsx
+++ frontend/src/routes/posts/ViewFeed.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Link, Navigate} from 'react-router-dom';
+import {Context} from '../../shared/Context';
 
 const ViewFeed: React.FC = () => {
+    const {state} = useContext(Context);
     return (
         <div>
+            {state.loggedIn || <Navigate to="/accounts/login" />}
             <Link to='/lol'>Lol</Link>
         </div>
     );
