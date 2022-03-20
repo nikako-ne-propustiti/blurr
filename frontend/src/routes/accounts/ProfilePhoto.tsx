c10de40fbe01d263ab1221fa0125184dd2ba5d02
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-03-20 10:19:38 +0100

    ProfilePhoto doesn't need any state

--- frontend/src/routes/accounts/ProfilePhoto.tsx
+++ frontend/src/routes/accounts/ProfilePhoto.tsx
@@ -1,78 +1,17 @@
 import React from 'react';
-import loader from '../../loader.gif';
 import './ProfilePhoto.css'
 
 interface Props {
-    username?: string
+    username?: string,
+    profilePhotoURL?: string,
+    canChange?: boolean
 };
 
-const ProfilePhoto: React.FC<Props> = ({ username }) => {
-    type ACTIONTYPE =
-        | { type: "fetch_init"; }
-        | { type: "fetch_success"; imageUrl: string; canUpload: boolean; }
-        | { type: "fetch_fail"; };
-
-    const initialState = {
-        imageUrl: loader,
-        tooltipText: 'Loading...',
-        isLoading: true,
-        isError: false,
-        canUpload: false,
-        newImageUrl: ''
-    };
-
-    const profilePhotoReducer = (state: typeof initialState, action: ACTIONTYPE) => {
-        switch (action.type) {
-            case 'fetch_init':
-                return {
-                    ...state,
-                    isLoading: true,
-                    isError: false
-                };
-            case 'fetch_success':
-                return {
-                    ...state,
-                    isLoading: false,
-                    isError: false,
-                    imageUrl: action.imageUrl,
-                    canUpload: action.canUpload,
-                    tooltipText: (action.canUpload) ? 'Click here to upload a new profile photo' : `${username}'s profile photo`
-                };
-            case 'fetch_fail':
-                return {
-                    ...state,
-                    isError: true,
-                    imageUrl: 'error.png',
-                    tooltipText: 'Error loading profile photo'
-                };
-            default:
-                throw new Error();
-        }
-    };
-
-    const [state, dispatch] = React.useReducer(profilePhotoReducer, initialState);
-
-    React.useEffect(() => {
-        dispatch({
-            type: 'fetch_init'
-        });
-        setTimeout(() => {
-            if (username !== 'noacc')
-                dispatch({
-                    type: 'fetch_success',
-                    imageUrl: 'https://picsum.photos/150/150',
-                    canUpload: false
-                });
-            else
-                dispatch({
-                    type: 'fetch_fail'
-                });
-        }, 1000);
-    }, [username]);
-
-
+// Profile photo, as it appears on the user's page
+// TODO Callback for changing profile photo, should be handled by parent component
+const ProfilePhoto: React.FC<Props> = ({ username, profilePhotoURL, canChange }) => {
     return <div className='profile-photo'>
-        <img src={state.imageUrl} title={state.tooltipText} />
+        <img src={profilePhotoURL} title={username} />
     </div>
 };
 
