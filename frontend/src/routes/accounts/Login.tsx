0206f198e8d1010111c40976ff3a5ccef9cd5a31
Luka Simić <git@kocka.tech>
2022-03-20 10:30:27 +0100

    Length limit and password in AccountTextInput.

--- frontend/src/routes/accounts/Login.tsx
+++ frontend/src/routes/accounts/Login.tsx
@@ -41,8 +41,8 @@ const Login: React.FC = () => {
     return <form action="/accounts/login" method="POST" className="login-form" onSubmit={onLoginSubmit}>
         {state.loggedIn && <Navigate to={returnTo} />}
         <img src={logo} alt="Blurr logo" />
-        <AccountTextInput name="username" placeholder="Username" />
-        <AccountTextInput name="password" placeholder="Password" />
+        <AccountTextInput name="username" placeholder="Username" length={30} />
+        <AccountTextInput name="password" placeholder="Password" length={255} />
         <Button text="Login" />
         <SubmissionIndicator submissionState={submissionState} text={indicatorText} />
         <p>No account? <strong><Link to={`/accounts/register?returnTo=${encodeURIComponent(returnTo)}`}>Register</Link></strong></p>
