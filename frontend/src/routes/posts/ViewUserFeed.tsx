3b638584f8252a1fe0947a1be04e05fa66920d50
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-03-20 22:23:29 +0100

    ViewUserFeed: Refactor state and buttons

--- frontend/src/routes/posts/ViewUserFeed.tsx
+++ frontend/src/routes/posts/ViewUserFeed.tsx
@@ -1,47 +1,65 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import User from '../../models/User';
 import PostBasicInfo from '../../models/PostBasicInfo';
 
 import { ProfilePhoto } from '../accounts';
 import PostGrid from './PostGrid';
+import { Context } from '../../shared/Context';
+import Button from '../../shared/Button';
 
 import './ViewUserFeed.css';
+
 
 const pluralHelper = (word: string, count: number | undefined) =>
     <><b>{count}</b> {`${word}${(count !== 1) ? 's' : ''}`}</>;
 
 type LoadState = 'INIT' | 'LOADED' | 'ERROR' | 'NOUSER';
 
-interface UserFeedState {
-    loadState: LoadState;
-    userInfo?: User;
-    posts?: PostBasicInfo[];
-}
 
 const ViewUserFeed: React.FC = () => {
     const { username } = useParams();
+    const { state: context } = React.useContext(Context);
+    const navigate = useNavigate();
+    const [loadState, setLoadState] = React.useState<LoadState>('INIT');
+    const [userInfo, setUserInfo] = React.useState<User>();
+    const [posts, setPosts] = React.useState<PostBasicInfo[]>([]);
 
-    const [state, setState] = React.useState<UserFeedState>({ loadState: 'INIT' });
+    const handleFollow = () => {
+        if (userInfo && context.loggedIn && username !== context.currentUser) {
+            if (userInfo.amFollowing)
+                setUserInfo({ ...userInfo, amFollowing: false });
+            else setUserInfo({ ...userInfo, amFollowing: true });
+        }
+
+        // TODO API call...
+    };
+
+    const handleEditAccount = () => {
+        navigate('/accounts/edit');
+    };
 
     React.useEffect(() => {
-        setState({ loadState: 'INIT' });
-
-        if (!username?.includes('error'))
-            setState({
-                loadState: 'LOADED',
-                userInfo: {
-                    username: username || '',
-                    profileURL: '',
-                    profilePhotoURL: 'http://picsum.photos/512/512?nocache=1',
-                    amFollowing: false,
-                    numberFollowing: 100,
-                    numberOfFollowers: 1,
-                    numberOfPosts: 20,
-                    realName: 'John Doe'
-                },
-                posts: [{
+        setLoadState('INIT');
+        if (username?.includes('error'))
+            setLoadState('ERROR');
+        else if (username?.includes('noacc'))
+            setLoadState('NOUSER');
+        else {
+            setUserInfo({
+                username: username || '',
+                profileURL: '',
+                profilePhotoURL: 'http://picsum.photos/512/512?nocache=1',
+                amFollowing: false,
+                numberFollowing: 100,
+                numberOfFollowers: 1,
+                numberOfPosts: 20,
+                realName: 'John Doe'
+            });
+            setPosts([
+                {
                     postID: 'testpost1',
                     photoURL: 'http://picsum.photos/512/512?nocache=2',
                     numberOfLikes: 2
@@ -53,12 +71,10 @@ const ViewUserFeed: React.FC = () => {
                     postID: 'testpost3',
                     photoURL: 'http://picsum.photos/512/512?nocache=4',
                     numberOfLikes: 38
-                }]
-            });
-        else setState({ loadState: 'NOUSER' });
+                }
+            ]);
+        }
     }, [username]);
-
-    const { loadState, userInfo, posts } = state;
 
     return <>
         <section className="userfeed-profile-info">
@@ -73,6 +89,13 @@ const ViewUserFeed: React.FC = () => {
                         <li><b>{userInfo.numberFollowing}</b> following</li>
                         <li>{pluralHelper('post', userInfo.numberOfPosts)}</li>
                     </ul></>}
+            </div>
+            <div>
+                {context.loggedIn
+                    && context.currentUser !== username
+                    && <Button text={(userInfo?.amFollowing) ? 'Unfollow' : 'Follow'} onClick={handleFollow} />}
+                {context.currentUser === username
+                    && <Button text='Edit account' onClick={handleEditAccount} />}
             </div>
         </section>
         <hr />
