d91365ba2e66167238f7782ae278bdeb9097a3d0
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-03-20 10:59:55 +0100

    Refactoring post grid and preview

--- frontend/src/routes/posts/PostGrid.tsx
+++ frontend/src/routes/posts/PostGrid.tsx
@@ -1,78 +1,17 @@
 import React from 'react';
 import PostPreview from './PostPreview';
 import './PostGrid.css';
+import Post from '../../models/Post';
 
 interface Props {
-    username?: string
+    posts: Post[]
+    loadMorePosts(lastID : string) : void // Callback to load more posts
 };
 
-const PostGrid: React.FC<Props> = ({ username }) => {
-    interface State {
-        isLoading: boolean,
-        isError: boolean,
-        lastLoaded?: string,
-        posts: string[]
-    }
-
-    type ACTIONTYPE =
-        | { type: "fetch_init"; }
-        | { type: "fetch_load"; posts: string[]; }
-        | { type: "fetch_fail"; };
-
-    const initialState: State = {
-        isLoading: true,
-        isError: false,
-        posts: []
-    };
-
-    const PostGridReducer = (state: typeof initialState, action: ACTIONTYPE) => {
-        switch (action.type) {
-            case 'fetch_init':
-                return {
-                    ...state,
-                    isLoading: true,
-                    isError: false
-                };
-            case 'fetch_load':
-                return {
-                    ...state,
-                    isLoading: false,
-                    isError: false,
-                    posts: state.posts.concat(action.posts)
-                };
-            case 'fetch_fail':
-                return {
-                    ...state,
-                    isError: true,
-                    imageUrl: ''
-                };
-            default:
-                throw new Error();
-        }
-    };
-
-    const [state, dispatch] = React.useReducer(PostGridReducer, initialState);
-
-    React.useEffect(() => {
-        dispatch({
-            type: 'fetch_init'
-        });
-        setTimeout(() => {
-            if (username !== 'noacc')
-                dispatch({
-                    type: 'fetch_load',
-                    posts: Array.from({ length: 5 }, () => Math.floor(Math.random() * 40).toString())
-                });
-            else
-                dispatch({
-                    type: 'fetch_fail'
-                });
-        }, 100 * Math.random());
-    }, [username]);
-
+const PostGrid: React.FC<Props> = ({ posts }) => {
     return <section className='post-grid'>
-        {!state.isError && state.posts.map((post) =>
-            <PostPreview photoId={post} key={post}></PostPreview>
+        {posts.map((post) =>
+            <PostPreview post={post} key={post.postID}></PostPreview>
         )}
     </section>
 };
--- frontend/src/routes/posts/PostPreview.tsx
+++ frontend/src/routes/posts/PostPreview.tsx
@@ -1,76 +1,18 @@
 import React from 'react';
-import loader from '../../loader.gif';
+import { Link } from 'react-router-dom';
 import './PostPreview.css';
+import Post from '../../models/Post';
 
 interface Props {
-    photoId? : string
+    post : Post
 };
 
-const PostPreview : React.FC<Props> = ({photoId}) => {
-    type ACTIONTYPE =
-    | { type: "fetch_init"; }
-    | { type: "fetch_success"; imageUrl: string; }
-    | { type: "fetch_fail"; };
-
-    const initialState = {
-        isLoading : true,
-        isError : false,
-        imageUrl : loader
-    };
-
-    const PostPreviewReducer = (state: typeof initialState, action: ACTIONTYPE) => {
-        switch (action.type) {
-            case 'fetch_init':
-                return {
-                    ...state,
-                    isLoading: true,
-                    isError: false
-                };
-            case 'fetch_success':
-                return {
-                    ...state,
-                    isLoading: false,
-                    isError: false,
-                    imageUrl : action.imageUrl
-                };
-            case 'fetch_fail':
-                return {
-                    ...state,
-                    isError: true,
-                    imageUrl: ''
-                };
-            default:
-                throw new Error();
-        }
-    };
-
-    const [state, dispatch] = React.useReducer(PostPreviewReducer, initialState);
-
-    React.useEffect(() => {
-        dispatch({
-            type: 'fetch_init'
-        });
-        setTimeout(() => {
-            if (photoId !== 'nophoto')
-                dispatch({
-                    type: 'fetch_success',
-                    imageUrl: `https://picsum.photos/512/512/?blur=${(Math.random()*10).toFixed()}&nocache=${Math.random()}`
-                });
-            else 
-            dispatch({
-                type: 'fetch_fail'
-            });
-        }, 1000 * Math.random());
-    }, [photoId]);
-
-    const openPostPage = () => {
-        if (state.isError || state.isLoading) return;
-        window.open(`/p/${state.imageUrl}`,'_blank');
-    };
-
-    return <div className='post-preview'>
-        <img className="post-preview-image" src={state.imageUrl} onClick={openPostPage} />
-    </div>
+// Square preview of a post, as it appears on the user's page
+const PostPreview: React.FC<Props> = ({ post }) => {
+    const {postID, photoURL} = post;
+    return <Link to={`/p/${postID}`} className='post-preview'>
+        <img className="post-preview-image" src={photoURL} />
+    </Link>
 };
 
 export default PostPreview;
