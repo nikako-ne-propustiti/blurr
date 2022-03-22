c782368ecce6f8d55d738adf0375c6c3e20ce109
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-03-22 18:29:28 +0100

    ViewUserFeed: Infinite scrolling + other edits

--- frontend/src/routes/posts/ViewUserFeed.tsx
+++ frontend/src/routes/posts/ViewUserFeed.tsx
@@ -9,13 +9,14 @@ import { ProfilePhoto } from '../accounts';
 import PostGrid from './PostGrid';
 import { Context } from '../../shared/Context';
 import Button from '../../shared/Button';
+import InfiniteScroll from '../../shared/InfiniteScroll';
 
 import './ViewUserFeed.css';
 
 const mockUserInfo: User = {
     username: 'not loaded',
     profileURL: '',
-    profilePhotoURL: 'http://picsum.photos/512/512?nocache=1',
+    profilePhotoURL: 'http://picsum.photos/512/512',
     amFollowing: false,
     numberFollowing: 100,
     numberOfFollowers: 1,
@@ -23,12 +24,12 @@ const mockUserInfo: User = {
     realName: 'John Doe'
 };
 
-const generateMockPost = () => {
-    return {
+const generateMockPosts = ( number : number) => {
+    return new Array(number).fill(null).map(() => {return {
         postID: Math.round((100000 * Math.random())).toFixed(),
         photoURL: `http://picsum.photos/512/512?blur=${Math.round((10 * Math.random())).toFixed()}&nocache=${Math.random()}`,
         numberOfLikes: Math.round((100 * Math.random()))
-    }
+    }});
 }
 
 const pluralHelper = (word: string, count: number | undefined) =>
@@ -43,7 +44,6 @@ const ViewUserFeed: React.FC = () => {
     const [loadState, setLoadState] = React.useState<LoadState>('INIT');
     const [userInfo, setUserInfo] = React.useState<User>();
     const [posts, setPosts] = React.useState<PostBasicInfo[]>([]);
-    let loaded = false;
 
     const handleFollow = () => {
         if (userInfo && context.loggedIn && username !== context.currentUser) {
@@ -51,12 +51,17 @@ const ViewUserFeed: React.FC = () => {
                 setUserInfo({ ...userInfo, amFollowing: false });
             else setUserInfo({ ...userInfo, amFollowing: true });
         }
-
         // TODO API call...
     };
 
+    // Edit account button
     const handleEditAccount = () => {
         navigate('/accounts/edit');
+    };
+
+    // Infinite scrolling callback
+    const handleInfiniteScroll = () => {
+        setPosts(posts.concat(generateMockPosts(10)));
     };
 
     React.useEffect(() => {
@@ -72,13 +77,14 @@ const ViewUserFeed: React.FC = () => {
                 ...mockUserInfo,
                 username: username || ''
             });
-            setPosts(new Array(10).fill(null).map(generateMockPost));
+            setPosts(generateMockPosts(10));
         }
     }, [username]);
 
     return <>
         <section className="userfeed-profile-info">
-            {userInfo && <ProfilePhoto user={userInfo} isMyProfile={true} />}
+            <InfiniteScroll callback={handleInfiniteScroll} />
+            {userInfo && <ProfilePhoto user={userInfo} isMyProfile={context.currentUser === username} />}
             <div className="userfeed-info">
                 {loadState === 'ERROR' && <p>Sorry, something went wrong...</p>}
                 {loadState === 'NOUSER' && <p>The requested user does not exist.</p>}
@@ -90,7 +96,7 @@ const ViewUserFeed: React.FC = () => {
                         <li>{pluralHelper('post', userInfo.numberOfPosts)}</li>
                     </ul></>}
             </div>
-            <div>
+            <div className='userfeed-buttons'>
                 {context.loggedIn
                     && context.currentUser !== username
                     && <Button text={(userInfo?.amFollowing) ? 'Unfollow' : 'Follow'} onClick={handleFollow} />}
