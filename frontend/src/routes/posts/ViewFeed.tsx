fce4a2837dd3ecd23bbc936cbd8e1c0a2418c743
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-03-25 18:48:24 +0100

    Basic feed functionality

--- frontend/src/routes/posts/ViewFeed.tsx
+++ frontend/src/routes/posts/ViewFeed.tsx
@@ -1,14 +1,64 @@
-import React, {useContext} from 'react';
-import {Link, Navigate} from 'react-router-dom';
-import {Context} from '../../shared/Context';
+import React, { useContext } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import ShowPost from './ShowPost';
+import { Post, User } from '../../models';
+import { Context } from '../../shared/Context';
+import InfiniteScroll from '../../shared/InfiniteScroll';
+
+import './ViewFeed.css';
+
+const generateMockUser = () : User => {
+        const id = Math.round((100000 * Math.random()));
+        return {
+            id: id,
+            username: `acc-${id}`,
+            profileURL : `acc-${id}`,
+            profilePhotoURL: `http://picsum.photos/128/128?nocache=${Math.random()}`,
+            amFollowing: false,
+            numberFollowing: Math.round((1000 * Math.random())),
+            numberOfFollowers: Math.round((1000 * Math.random())),
+            numberOfPosts: Math.round((1000 * Math.random())),
+            realName: 'John Doe'
+        }
+}
+
+const generateMockPosts = (number: number) : Post[] => {
+    return new Array(number).fill(null).map(() => {
+        return {
+            id: Math.round((100000 * Math.random())).toFixed(),
+            photoURL: `http://picsum.photos/512/512?blur=${Math.round((10 * Math.random())).toFixed()}&nocache=${Math.random()}`,
+            description: 'Description',
+            haveLiked: Math.random() < 0.5,
+            time: new Date(),
+            poster: generateMockUser(),
+            comments: []
+        }
+    });
+}
 
 const ViewFeed: React.FC = () => {
-    const {state} = useContext(Context);
+    const { state: context } = useContext(Context);
+    const [posts, setPosts] = React.useState<Post[]>([]);
+
+    React.useEffect(() => {
+        setPosts(generateMockPosts(5));
+    }, []);
+
+    // Infinite scrolling callback
+    const handleInfiniteScroll = React.useCallback(() => {
+        setPosts(posts.concat(generateMockPosts(10)));
+    }, [posts]);
+
     return (
-        <div>
-            {state.loggedIn || <Navigate to="/accounts/login" />}
-            <Link to='/lol'>Lol</Link>
-        </div>
+        <>
+            {context.loggedIn || <Navigate to="/accounts/login" />}
+            <InfiniteScroll callback={handleInfiniteScroll} />
+            <section className="feed-list">
+                {posts.map((post) =>
+                    <ShowPost post={post} key={post.id} />
+                )}
+            </section>
+        </>
     );
 }
 
