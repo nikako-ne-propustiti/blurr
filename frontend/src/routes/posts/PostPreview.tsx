e9fdd7974bbf7fa05c09ad79aea3a496a5cd949c
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2022-05-30 18:17:39 +0200

    Remove unused import

--- frontend/src/routes/posts/PostPreview.tsx
+++ frontend/src/routes/posts/PostPreview.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {Post} from '../../models';
 import {BACKEND_API_URL} from '../../api';
