de8319d9e26f95356d0dac857741fddbef4b2db4
Luka Simić <git@kocka.tech>
2022-05-30 23:05:30 +0200

    Add useful test utility methods to test_helper.

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -3,8 +3,31 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def assert_request(opts)
+    assert_equal(@response.body, opts[:body].to_json)
+    assert_equal(@response.status, (opts[:status] || 200))
+  end
+
+  def assert_already_exists
+    assert_request body: {
+      success: false,
+      error: 'Already exists.'
+    }, status: 400
+  end
+
+  def assert_missing_param
+    assert_request body: {
+      success: false,
+      error: 'Missing required fields.'
+    }, status: 400
+  end
+
+  def assert_not_found
+    assert_request body: {
+      success: false,
+      error: 'Requested resource does not exist.'
+    }, status: 404
+  end
 end
