3eb03612987084c94abe3af83bce8510bb0c0b33
Ivan Pešić <ivan.pesic2000@gmail.com>
2022-06-05 13:17:59 +0200

    Write tests for review controller

--- backend/test/controllers/review_controller_test.rb
+++ backend/test/controllers/review_controller_test.rb
@@ -7,42 +7,66 @@ class ReviewControllerTest < ActionDispatch::IntegrationTest
 
     db_post = Post.find_by(post_url: 'url1')
     test_post = {
-      # Ignore
       id: db_post[:id],
-      post_url: 'posturl',
-      file_uuid: '285b9102-fb53-4f9b-9c6a-46198c689825',
+      url: 'url1',
+      photoURL: '/images/285b9102-fb53-4f9b-9c6a-46198c689825.jpg',
+      reviewPhotoURL: '/images/285b9102-fb53-4f9b-9c6a-46198c689825pass1.jpg',
       description: 'desc1',
-      user_id: get_regular_user.id,
-      # Ignore
-      created_at: db_post[:created_at],
-      password: '123',
-      reviewed: false,
-      sponsored: false,
+      haveLiked: false,
+      followingWhoLiked:[],
+      likes: 0,
+      time: db_post[:created_at],
+      poster: {
+        id: 336453508,
+        username: 'testuser1',
+        realName: 'Test User1',
+        profileURL: 'testuser1',
+        profilePhotoURL: '/default_images/default_user.jpg',
+        amFollowing: false,
+        numberOfPosts: 2,
+        numberOfFollowers: 0,
+        numberFollowing: 0
+      },
+      comments: [],
+      unlocked: false
     }
 
     assert_request body: {
       success: true,
-      posts: [test_post.to_json]
+      posts: [test_post]
     }
   end
 
   test "approve post" do
       login_admin_user
-      post api_review_path(get_unreviewed_post.id), params: {
+      post "/api/review/#{get_unreviewed_post.id}", params: {
         approve: true
       }
       assert_request body: {
         success: true,
       }
-      # TODO assert da je post.reviewed == true nakon ovoga
+      approved_post = Post.find_by(id: get_unreviewed_post.id)
+
+      assert_equal(approved_post.reviewed, true)
   end
 
   test "disapprove post" do
     login_admin_user
-    post api_review_path(get_unreviewed_post.id)
+    post "/api/review/#{get_unreviewed_post.id}", params: {
+      approve: false,
+    }, as: :json
     assert_request body: {
       success: true,
     }
-    # TODO assert da je post izbrisan nakon ovoga
+    disapproved_post = Post.find_by(id: get_unreviewed_post.id)
+
+    assert_nil disapproved_post
+  end
+
+  test "regular user cannot review posts" do
+    login_regular_user
+    get api_review_path
+
+    assert_require_admin
   end
 end
