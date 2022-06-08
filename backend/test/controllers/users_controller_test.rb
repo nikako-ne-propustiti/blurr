8424b5d4d07c88fb01db044b1ab4e320647efb0f
Ivan Pešić <ivan.pesic2000@gmail.com>
2022-06-08 19:41:29 +0200

    Add account info unit tests

--- backend/test/controllers/users_controller_test.rb
+++ backend/test/controllers/users_controller_test.rb
@@ -37,4 +37,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     }
     assert Follow.exists?(follower_id: get_user3.id, followee_id: get_user4.id)
   end
+
+  test "get account info unsuccessful non-existing user" do
+    get "/api/users/nonexisting"
+    assert_not_found
+  end
+
+  test "get account info successfully" do
+    get "/api/users/#{get_user3.username}"
+    assert_request body: {
+      success: true,
+      account: {
+        id: get_user3.id,
+        username: "testuser3",
+        realName: "Test User3",
+        profileURL: "testuser3",
+        profilePhotoURL: "/default_images/default_user.jpg",
+        amFollowing: false,
+        numberOfPosts: 1,
+        numberOfFollowers: 0,
+        numberFollowing: 1,
+      }
+    }
+  end
 end
